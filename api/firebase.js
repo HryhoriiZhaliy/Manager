e70e07b3af9187b898af4bb7a3e700ca854b8ea1
HryhoriiZhaliy <zhalih00@jcu.cz>
2025-05-03 17:56:44 +0000

    1

--- api/firebase.js
+++ api/firebase.js
@@ -4,14 +4,13 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://manager-9195f-default-rtdb.europe-west1.firebasedatabase.app", // важно
-  projectId: "manager-9195f",
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-
 export default db;
--- api/tasks.js
+++ api/tasks.js
@@ -14,11 +14,10 @@ export default async function handler(req, res) {
 
       res.status(200).json(tasks);
     } catch (err) {
-      console.error('DB Error:', err);
-      res.status(500).json({ message: 'Failed to load tasks' });
+      console.error('DB ERROR:', err);
+      res.status(500).json({ message: 'Failed to fetch tasks', error: err.message });
     }
   } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
-
